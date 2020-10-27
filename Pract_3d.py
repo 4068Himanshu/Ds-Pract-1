0eec41f9369d8e7803d97a7042709069e2a9ca22
4068Himanshu <60869801+4068Himanshu@users.noreply.github.com>
2020-10-27 21:04:00 +0530

    Update Pract_3d.py

--- Pract_3d.py
+++ Pract_3d.py
@@ -1,41 +1,44 @@
-def recur_factorial(n):  
-   if n == 1:  
-       return n  
-   else:  
-       return n*recur_factorial(n-1)  
- 
-num = int(input("Enter a number: "))  
+# Python Program to find Factorial of a Number Using Recursive And Iterative Function.
+def recur_factorial(n):
 
-if num < 0:  
-   print("Sorry, factorial does not exist for negative numbers")  
-elif num == 0:  
-   print("The factorial of 0 is 1")  
-else:  
-   print("The factorial of",num,"is",recur_factorial(num))
+    if n == 1:
 
-def factorial(n):
+        return n
 
-	fact = 1
-	for i in range(1, n + 1):
-		fact = fact * i
+    else:
 
-	return fact
+        return n*recur_factorial(n-1)
 
 
-if __name__ == '_main_':
-    print("The Factorial of", n, "is", factorial(n))
 
-#using iteration
-def fact(number):
-   
-    fact = 1
+num=int(input("Enter the number: "))
+
+
+
+print("factorial of ",num," (recursive): ",end="")
+
+print(recur_factorial(num))
+
+
+
+def iter_factorial(n):
+
+    factorial=1
+
+    if int(n) >= 1:
+
+        for i in range (1,int(n)+1):
+
+            factorial = factorial * i
+
+        return factorial
+
   
-    for number in range(5, 1,-1):
-    
-        fact = fact * number
-    return fact
 
-number = int(input("Enter a number for iteration : "))
+num=int(input("Enter the number: "))
 
-factorial = fact(number)
-print("Factorial is "+str(factorial))
+
+
+print("factorial of ",num," (iterative): ",end="")
+
+print(iter_factorial(num))
